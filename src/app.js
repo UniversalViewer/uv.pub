4ed633ab753ad7217119ff30b642ab1b8af771de
edsilv <e.silverton@gmail.com>
2018-06-01 11:12:43 +0100

    improved ux

--- src/app.js
+++ src/app.js
@@ -4,14 +4,12 @@ import "./stylesheets/main.css";
 import "./helpers/context_menu.js";
 import "./helpers/external_links.js";
 
-// ----------------------------------------------------------------------------
-// Everything below is just to show you how it works. You can delete all of it.
-// ----------------------------------------------------------------------------
-
 import { remote } from "electron";
 import jetpack from "fs-jetpack";
 import { build } from "biiif";
 import env from "env";
+const Dat = require('dat-node');
+const urljoin = require('url-join');
 
 const app = remote.app;
 const appDir = jetpack.cwd(app.getAppPath());
@@ -23,14 +21,19 @@ const osMap = {
 	linux: "Linux"
 };
 
-let directoryPath;
+const datgateway = 'http://174.138.105.19:3000';
+let directorypath;
 let appContainer = document.querySelector("#app");
 let dragArea = document.querySelector('#dragarea');
 let form = document.querySelector('#form1');
-let url = document.querySelector('#url');
+//let url = document.querySelector('#url');
+let datlinkinput = document.querySelector('#dat-link-input');
+let dragiiif = document.querySelector('#drag-iiif');
 let submit = document.querySelector('#submit');
+let foldername;
 
 appContainer.style.display = "block";
+datlinkinput.style.display = "none";
 
 document.addEventListener('drop', function (e) {
 	e.preventDefault();
@@ -38,8 +41,9 @@ document.addEventListener('drop', function (e) {
 
 	for (let f of e.dataTransfer.files) {
 		console.log('dragged: ', f.path);
-		directoryPath = f.path;
-		dragArea.innerHTML = f.name;
+		directorypath = f.path;
+		foldername = f.name;
+		dragArea.innerHTML = '<p>Click below to generate and share your IIIF!</p>';
 	}
 });
 
@@ -50,29 +54,49 @@ document.addEventListener('dragover', function (e) {
 
 form.onsubmit = function() {
 	
-	if (!url.value) {
-		alert('please enter a url');
-		return false;
-	} else if (!(directoryPath && jetpack.exists(directoryPath) && jetpack.inspect(directoryPath).type === "dir")) {
+	// if (!url.value) {
+	// 	alert('please enter a url');
+	// 	return false;
+	if (!(directorypath && jetpack.exists(directorypath) && jetpack.inspect(directorypath).type === 'dir')) {
 		alert('please drag a folder into the region above');
 		return false;
 	}
+
 	submit.disable = true;
-	
-	build(directoryPath, url.value);
+
+	Dat(directorypath, function(err, dat) {
+		
+		if (err) {
+			alert(err);
+			return;
+		}
+
+		const datkey = dat.key.toString('hex');
+		const datlink = urljoin(datgateway, datkey);
+
+		build(directorypath, datlink, datkey);
+	  
+		dat.importFiles();
+		dat.joinNetwork();
+
+		const manifestUrl = urljoin(datlink, 'index.json');
+		dragiiif.style.display = 'block';
+		dragiiif.href = manifestUrl + '?manifest=' + manifestUrl;
+		datlinkinput.value = manifestUrl;
+		datlinkinput.style.display = 'block';
+		dragArea.innerHTML = '<p>Done! Drag the IIIF logo into a viewer, or copy and paste the URL.</p>';
+	});
 
 	submit.disable = false;
 
-	alert('Done!');
+	//alert('Done!');
 
 	return false; // don't need to actually submit it.
 }
 
 /*
-document.querySelector("#greet").innerHTML = greet();
 document.querySelector("#os").innerHTML = osMap[process.platform];
 document.querySelector("#author").innerHTML = manifest.author;
 document.querySelector("#env").innerHTML = env.name;
-document.querySelector("#electron-version").innerHTML =
-  process.versions.electron;
+document.querySelector("#electron-version").innerHTML = process.versions.electron;
 */

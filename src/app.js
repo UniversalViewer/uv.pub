2eb22d32ef352ad8c24161f5e3a56c0c869bb34a
edsilv <e.silverton@gmail.com>
2018-11-18 23:08:13 +0000

    added dat icon

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import { remote } from "electron";
 import jetpack from "fs-jetpack";
 import { build } from "biiif";
 import env from "env";
+import { fips } from "crypto";
 const Dat = require('dat-node');
 const urljoin = require('url-join');
 
@@ -21,30 +22,30 @@ const osMap = {
 	linux: "Linux"
 };
 
-const datgateway = 'http://174.138.105.19:3000'; // damahub
-//const datgateway = 'http://142.93.234.230:3000'; // ed
-let directorypath;
-let appContainer = document.querySelector("#app");
-let dragArea = document.querySelector('#dragarea');
-let form = document.querySelector('#form1');
-//let url = document.querySelector('#url');
-let datlinkinput = document.querySelector('#dat-link-input');
-let dragiiif = document.querySelector('#drag-iiif');
-let submit = document.querySelector('#submit');
+const datgateway = env.dat.gateway;
+let directorypath, datkey, daturl;
+const appContainer = document.querySelector("#app");
+const dragArea = document.querySelector('#dragarea');
+const results = document.querySelector('#results');
+//const form = document.querySelector('#form1');
+//const url = document.querySelector('#url');
+const gatewaylink = document.querySelector('#dat-link-input');
+const dragiiificon = document.querySelector('#drag-iiif');
+const datkeyicon = document.querySelector('#dat-key');
+//const submit = document.querySelector('#submit');
 
 appContainer.style.display = "block";
-datlinkinput.style.display = "none";
 
 document.addEventListener('drop', function (e) {
 	e.preventDefault();
 	e.stopPropagation();
-
 	for (let f of e.dataTransfer.files) {
 		console.log('dragged: ', f.path);
 		directorypath = f.path;
 		//foldername = f.name;
-		dragArea.innerHTML = '<p>Click below to generate and share your IIIF!</p>';
+		//dragArea.innerHTML = '<p>Click below to generate and share your IIIF!</p>';
 	}
+	directoryDropped();
 });
 
 document.addEventListener('dragover', function (e) {
@@ -52,6 +53,73 @@ document.addEventListener('dragover', function (e) {
 	e.stopPropagation();
 });
 
+datkeyicon.addEventListener('click', function(e) {
+	e.preventDefault();
+	setClipboard(datkey);
+	return false;
+});
+
+function directoryDropped() {
+
+	results.style.display = 'none';
+
+	if (!(directorypath && jetpack.exists(directorypath) && jetpack.inspect(directorypath).type === 'dir')) {
+		alert('please drag a folder into the region above');
+		return false;
+	}
+
+	// working...
+	// const workingInterval = window.setInterval(() => {
+	// 	if (dragArea.innerHTML.length > 3) {
+	// 		dragArea.innerHTML = "";
+	// 	} else {
+	// 		dragArea.innerHTML += ".";
+	// 	}
+	// }, 100);
+
+	dragArea.innerHTML = '<p>Working</p>';
+
+	Dat(directorypath, function(err, dat) {
+		
+		if (err) {
+			alert(err);
+			return;
+		}
+
+		datkey = dat.key.toString('hex');
+		daturl = urljoin(datgateway, datkey);
+
+		build(directorypath, daturl, true, datkey).then(() => {
+
+			dat.importFiles();
+			dat.joinNetwork();
+
+			const manifestUrl = urljoin(daturl, 'index.json');
+			dragiiificon.href = manifestUrl + '?manifest=' + manifestUrl;
+			gatewaylink.value = manifestUrl;
+			datkeyicon.href = 'dat://' + datkey;
+			
+			results.style.display = 'block';
+
+			//clearInterval(workingInterval);
+			dragArea.innerHTML = '<p>Done! Drag the IIIF logo into a viewer, or copy and paste the URL.</p>';
+			//submit.disable = false;
+		});
+	  
+	});
+}
+
+function setClipboard(value) {
+    var tempInput = document.createElement("input");
+    tempInput.style = "position: absolute; left: -1000px; top: -1000px";
+    tempInput.value = value;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
+}
+
+/*
 form.onsubmit = () => {
 	
 	// if (!url.value) {
@@ -83,18 +151,18 @@ form.onsubmit = () => {
 		}
 
 		const datkey = dat.key.toString('hex');
-		const datlink = urljoin(datgateway, datkey);
+		const daturl = urljoin(datgateway, datkey);
 
-		build(directorypath, datlink, true, datkey).then(() => {
+		build(directorypath, daturl, true, datkey).then(() => {
 
 			dat.importFiles();
 			dat.joinNetwork();
 
-			const manifestUrl = urljoin(datlink, 'index.json');
+			const manifestUrl = urljoin(daturl, 'index.json');
 			dragiiif.style.display = 'block';
 			dragiiif.href = manifestUrl + '?manifest=' + manifestUrl;
-			datlinkinput.value = manifestUrl;
-			datlinkinput.style.display = 'block';
+			gatewaylink.value = manifestUrl;
+			gatewaylink.style.display = 'block';
 			
 			//clearInterval(workingInterval);
 			dragArea.innerHTML = '<p>Done! Drag the IIIF logo into a viewer, or copy and paste the URL.</p>';
@@ -106,6 +174,7 @@ form.onsubmit = () => {
 	return false; // don't need to actually submit it.
 
 }
+*/
 
 /*
 document.querySelector("#os").innerHTML = osMap[process.platform];

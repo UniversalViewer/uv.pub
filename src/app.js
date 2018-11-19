f72d5004fdb838ccb0688685c6b6bdd9e3b0aa70
edsilv <e.silverton@gmail.com>
2018-11-19 17:04:32 +0000

    click icons to copy

--- src/app.js
+++ src/app.js
@@ -4,13 +4,13 @@ import "./stylesheets/main.css";
 import "./helpers/context_menu.js";
 import "./helpers/external_links.js";
 
-import { remote } from "electron";
-import jetpack from "fs-jetpack";
-import { build } from "biiif";
-import env from "env";
-import { fips } from "crypto";
+const { basename, dirname, sep } = require('path');
 const Dat = require('dat-node');
 const urljoin = require('url-join');
+import { build } from "biiif";
+import { remote } from "electron";
+import env from "env";
+import jetpack from "fs-jetpack";
 
 const app = remote.app;
 const appDir = jetpack.cwd(app.getAppPath());
@@ -22,50 +22,77 @@ const osMap = {
 	linux: "Linux"
 };
 
-const datgateway = env.dat.gateway;
-let directorypath, datkey, daturl;
+const datGateway = env.dat.gateway;
+let dropPath, datKey, datUrl;
 const appContainer = document.querySelector("#app");
 const dragArea = document.querySelector('#dragarea');
 const results = document.querySelector('#results');
 //const form = document.querySelector('#form1');
 //const url = document.querySelector('#url');
-const gatewaylink = document.querySelector('#dat-link-input');
-const dragiiificon = document.querySelector('#drag-iiif');
-const datkeyicon = document.querySelector('#dat-key');
+const iiifIcon = document.querySelector('#iiif-icon');
+const httpPath = document.querySelector('#http-path');
+const datIcon = document.querySelector('#dat-icon');
+const datPath = document.querySelector('#dat-path');
 //const submit = document.querySelector('#submit');
 
-appContainer.style.display = "block";
+appContainer.style.display = 'block';
 
-document.addEventListener('drop', function (e) {
+document.addEventListener('drop', (e) => {
 	e.preventDefault();
 	e.stopPropagation();
 	for (let f of e.dataTransfer.files) {
 		console.log('dragged: ', f.path);
-		directorypath = f.path;
+		dropPath = f.path;
 		//foldername = f.name;
 		//dragArea.innerHTML = '<p>Click below to generate and share your IIIF!</p>';
 	}
-	directoryDropped();
+	dropped();
 });
 
-document.addEventListener('dragover', function (e) {
+document.addEventListener('dragover', (e) => {
 	e.preventDefault();
 	e.stopPropagation();
 });
 
-datkeyicon.addEventListener('click', function(e) {
+datIcon.addEventListener('click', (e) => {
 	e.preventDefault();
-	setClipboard(datkey);
+	setClipboard(datKey);
 	return false;
 });
 
-function directoryDropped() {
+const copyIcons = document.querySelectorAll('.copy-icon a');
+
+copyIcons.forEach((copyIcon) => {
+	copyIcon.addEventListener('click', function(e) {
+		e.preventDefault();
+		const input = this.parentElement.querySelector('input');
+		input.focus();
+		input.select();
+		document.execCommand('copy');
+	});
+});
+
+function isDirectory(path) {
+	return (path && jetpack.exists(path) && jetpack.inspect(path).type === 'dir');
+}
+
+function setClipboard(value) {
+    const tempInput = document.createElement('input');
+    tempInput.style = 'position: absolute; left: -1000px; top: -1000px';
+    tempInput.value = value;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand('copy');
+    document.body.removeChild(tempInput);
+}
+
+function dropped() {
 
 	results.style.display = 'none';
 
-	if (!(directorypath && jetpack.exists(directorypath) && jetpack.inspect(directorypath).type === 'dir')) {
-		alert('please drag a folder into the region above');
-		return false;
+	if (!isDirectory(dropPath)) {
+		// set dropPath to the parent directory
+		dropPath = dirname(dropPath);
 	}
 
 	// working...
@@ -79,97 +106,58 @@ function directoryDropped() {
 
 	dragArea.innerHTML = '<p>Working</p>';
 
-	Dat(directorypath, function(err, dat) {
+	Dat(dropPath, (err, dat) => {
 		
 		if (err) {
 			alert(err);
 			return;
+		} else {
+
+			datKey = dat.key.toString('hex');
+			datUrl = urljoin(datGateway, datKey);
+
+			build(dropPath, datUrl, true, datKey).then(() => {
+
+				dat.archive.on('error', () => {
+					alert(err);
+					return;
+				});
+
+				dat.importFiles(() => {
+
+					dat.joinNetwork();
+
+					const datUrl = 'dat://' + datKey;
+					datIcon.href = datUrl;
+					datPath.value = datUrl;
+
+					const httpUrl = urljoin(urljoin(datGateway, datKey), 'index.json');
+					iiifIcon.href = httpUrl + '?manifest=' + httpUrl;
+					httpPath.value = httpUrl;
+
+					results.style.display = 'block';
+
+					//clearInterval(workingInterval);
+					dragArea.innerHTML = '<p>Done! Drag the IIIF logo into a viewer, or copy and paste the URL.</p>';
+					//submit.disable = false;
+
+				});
+				
+			});
+
 		}
-
-		datkey = dat.key.toString('hex');
-		daturl = urljoin(datgateway, datkey);
-
-		build(directorypath, daturl, true, datkey).then(() => {
-
-			dat.importFiles();
-			dat.joinNetwork();
-
-			const manifestUrl = urljoin(daturl, 'index.json');
-			dragiiificon.href = manifestUrl + '?manifest=' + manifestUrl;
-			gatewaylink.value = manifestUrl;
-			datkeyicon.href = 'dat://' + datkey;
-			
-			results.style.display = 'block';
-
-			//clearInterval(workingInterval);
-			dragArea.innerHTML = '<p>Done! Drag the IIIF logo into a viewer, or copy and paste the URL.</p>';
-			//submit.disable = false;
-		});
-	  
 	});
-}
-
-function setClipboard(value) {
-    var tempInput = document.createElement("input");
-    tempInput.style = "position: absolute; left: -1000px; top: -1000px";
-    tempInput.value = value;
-    document.body.appendChild(tempInput);
-    tempInput.select();
-    document.execCommand("copy");
-    document.body.removeChild(tempInput);
 }
 
 /*
 form.onsubmit = () => {
 	
-	// if (!url.value) {
-	// 	alert('please enter a url');
-	// 	return false;
-	if (!(directorypath && jetpack.exists(directorypath) && jetpack.inspect(directorypath).type === 'dir')) {
+	if (!(dropPath && jetpack.exists(dropPath) && jetpack.inspect(dropPath).type === 'dir')) {
 		alert('please drag a folder into the region above');
 		return false;
 	}
 
-	// working...
-	// const workingInterval = window.setInterval(() => {
-	// 	if (dragArea.innerHTML.length > 3) {
-	// 		dragArea.innerHTML = "";
-	// 	} else {
-	// 		dragArea.innerHTML += ".";
-	// 	}
-	// }, 100);
-
-	dragArea.innerHTML = '<p>Working</p>';
-
 	submit.disable = true;
-
-	Dat(directorypath, function(err, dat) {
-		
-		if (err) {
-			alert(err);
-			return;
-		}
-
-		const datkey = dat.key.toString('hex');
-		const daturl = urljoin(datgateway, datkey);
-
-		build(directorypath, daturl, true, datkey).then(() => {
-
-			dat.importFiles();
-			dat.joinNetwork();
-
-			const manifestUrl = urljoin(daturl, 'index.json');
-			dragiiif.style.display = 'block';
-			dragiiif.href = manifestUrl + '?manifest=' + manifestUrl;
-			gatewaylink.value = manifestUrl;
-			gatewaylink.style.display = 'block';
-			
-			//clearInterval(workingInterval);
-			dragArea.innerHTML = '<p>Done! Drag the IIIF logo into a viewer, or copy and paste the URL.</p>';
-			submit.disable = false;
-		});
-	  
-	});
 
 	return false; // don't need to actually submit it.
 
